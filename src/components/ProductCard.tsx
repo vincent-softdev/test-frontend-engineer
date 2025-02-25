75797fb600aa6e295829d222aba09edce0952f81
vincent-softdev <vinh.ly.softdev@gmail.com>
2025-02-25 20:49:16 +1030

    4th: update the product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,13 +7,13 @@ type ProductCardProps = {
 
 const ProductCard = (props: ProductCardProps) => {
     return (
-        <div>
-            <img src={props.product.image} alt={props.product.title} />
-            <h2>{props.product.title}</h2>
-            <p>{props.product.price}</p>
+        <div className="border p-4 rounded shadow-md">
+            <img src={props.product.image} alt={props.product.title} className="h-40 object-cover mx-auto"/>
+            <h2 className="font-bold text-lg">{props.product.title}</h2>
+            <p className="text-gray-600">{props.product.price}</p>
             {/* Assume we already have this product page with id is a parameter */}
             <Link href={`/products/${props.product.id}`}>
-                <button>View Details</button>
+                <button className="mt-2 p-2 bg-green-500 text-white">View Details</button>
             </Link>
         </div>
     )
