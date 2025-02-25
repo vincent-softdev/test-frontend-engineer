97aee7e07f5568d7bd30dba88a72d32a28fce44f
vincent-softdev <vinh.ly.softdev@gmail.com>
2025-02-25 20:36:54 +1030

    Update name for browser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 
+export const metadata = {
+  title: "Product Listing",
+  description: "List of products with Next.js",
+}
+
 export default function Home() {
   // Initial product
-  
+
 
   return (
     <div>
