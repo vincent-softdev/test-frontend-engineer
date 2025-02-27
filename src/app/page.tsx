1887e4f1a7703543829744c4f36a38af6a0252eb
vincent-softdev <vinh.ly.softdev@gmail.com>
2025-02-28 00:08:11 +1030

    Empty Home page for later update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,12 @@
-import ProductCard from "@/components/ProductCard";
-import { ProductService } from "@/services/ProductService";
-import { IProduct } from "@/types";
-
 export const metadata = {
   title: "Home",
   description: "List of products with Next.js",
 }
 
 const HomePage = async () => {
-  // Initial product or load products
-  const products: IProduct[] = await ProductService.getProducts();
-
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Product Listing</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      This is Home page
     </div>
   );
 }
